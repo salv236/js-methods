eff2d25e6339745579cd64c36d26fbf933b2d4cb
salv236 <salvatoresaia@gmail.com>
2019-03-17 14:57:31 +0100

    completed exercises with the push method

---
+++ array-methods/js/push.js
@@ -0,0 +1,16 @@
+let animals = ['dog', 'cat', 'monkey', 'gerbil'];
+
+animals.pop();
+console.log(animals);
+
+// add to the end of the array
+
+animals.push('shark');
+
+console.log(animals);
+
+// adding multiple values to the end of the array
+
+animals.push('dolphin', 'crocodile', 'buffalo');
+
+console.log('the new range of animals is ' + animals);
