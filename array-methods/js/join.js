5fbfb3c3ec214f5db0e5accf1e3857b80bedee46
salv236 <salvatoresaia@gmail.com>
2019-03-17 17:08:12 +0100

    added exercise containing join method

---
+++ array-methods/js/join.js
@@ -0,0 +1,7 @@
+// join method, converts array entries into strings wtiht the addition that you can specify the separator to use
+
+let animals = ['dog', 'cat', 'monkey', 'gerbil'];  
+
+animals.join('!!');
+
+console.log(animals);
