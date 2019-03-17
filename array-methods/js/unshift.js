3e2f81924113bd6e254ad7df384c16761636d74d
salv236 <salvatoresaia@gmail.com>
2019-03-17 16:39:11 +0100

    inserted exercises with unshift

---
+++ array-methods/js/unshift.js
@@ -0,0 +1,17 @@
+// unshift method adds an entry at the beginning of the index
+
+let animals = ['dog', 'cat', 'monkey', 'gerbil'];  
+
+// guerilla = 0 dog = 1 car = 2 monkey = 3 gerbil = 4
+animals.unshift('guerilla');
+
+console.log(animals);
+
+// add multiple entries at the beginning of the array
+
+animals.unshift('horse', 'rabbit', 'pig');
+// horse = 0, rabbit = 1, pig = 2, guerilla = 3 dog = 4 car = 5 monkey = 6 gerbil = 7 
+console.log(animals)
+
+
+
