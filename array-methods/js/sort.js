655fa7f24c7178a2baae4483f81244fea5145a8a
salv236 <salvatoresaia@gmail.com>
2019-03-17 17:52:11 +0100

    issue not understanding outputs for the last 2 arrays

---
+++ array-methods/js/sort.js
@@ -0,0 +1,11 @@
+// sorts array content alphabetically or numerically
+
+let countries = ['italy', 'spain', 'hungary', 'ukraine'];
+let numbers = [10,5,1,9,6,4,2,0,3];
+let negativePositve = [2, -2, -5, -1, 3];
+
+console.log(countries.sort());
+//[0, 1, 10, 2, 3, 4, 5, 6, 9] ??
+console.log(numbers.sort());
+// [-1, -2, -5, 2, 3] ??
+console.log(negativePositve.sort());
