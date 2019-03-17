9149922f4db5c60f45d0d936f29e99d1a17110a0
salv236 <salvatoresaia@gmail.com>
2019-03-17 16:56:41 +0100

    completion of tostring exercise

---
+++ array-methods/js/tostring.js
@@ -0,0 +1,7 @@
+// tostring method converts the array into string values each separated with a comma.
+
+let animals = ['dog', 'cat', 'monkey', 'gerbil'];  
+
+animals.toString();
+
+console.log(animals.toString());
