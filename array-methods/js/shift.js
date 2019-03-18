e60480a8a379a59541e26f81c279d737cc67f14a
Judit Cs. Pacsai <34191321+jpacsai@users.noreply.github.com>
2019-03-18 23:20:22 +0100

    add comments

--- array-methods/js/shift.js
+++ array-methods/js/shift.js
@@ -2,8 +2,10 @@
 
 let animals = ['dog', 'cat', 'monkey', 'gerbil'];  
 
-animals.shift(); // cat  = 0, monkey = 1, gerbil = 2
+animals.shift(); // cat  = 0, monkey = 1, gerbil = 2    ---> you applied shift, removed 'dog'
+
 //outputs the first removed entry which should be dog
-console.log(animals.shift());
+console.log(animals.shift());   // ---> you applied shift the second time here, removed 'cat'
+
 // should print out cat, monkey, gerbil
 console.log(animals);
