eff31bdfab8f607cad3fb7e313691bd88c907b1f
salv236 <salvatoresaia@gmail.com>
2019-03-19 21:53:39 +0100

    corrected duplicate shift()

--- array-methods/js/shift.js
+++ array-methods/js/shift.js
@@ -1,9 +1,10 @@
 // removes the first item in the array which causes the indexes to be readapted
+// further reading - https://www.w3schools.com/jsref/jsref_shift.asp
 
 let animals = ['dog', 'cat', 'monkey', 'gerbil'];  
 
 animals.shift(); // cat  = 0, monkey = 1, gerbil = 2
-//outputs the first removed entry which should be dog
-console.log(animals.shift());
+
+
 // should print out cat, monkey, gerbil
 console.log(animals);
