70a39ac1a3d03453c8fce92b6510eefd6f8cb6cf
salv236 <salvatoresaia@gmail.com>
2019-03-20 08:27:36 +0100

    completed exercise using the charat method

---
+++ strings/js/charat.js
@@ -0,0 +1,14 @@
+// return a character of a string that is specified at a given position
+
+//EXAMPLE - returning the last string
+
+var str = "HELLO WORLD";
+var res = str.charAt(str.length-1);
+
+//EXERCISE
+
+let string = 'this is breaking news';
+
+let stringPosition = string.charAt(10);
+
+console.log(stringPosition);
