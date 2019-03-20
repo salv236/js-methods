dc1ed6a3226bbc8194baa863817312a726b3b2f2
salv236 <salvatoresaia@gmail.com>
2019-03-20 13:06:40 +0100

    added completed exerise string concatenation with concat()

---
+++ strings/js/concat.js
@@ -0,0 +1,8 @@
+var string1 = 'this';
+var string2 = 'is';
+var string3 = 'a';
+var string4 = 'string';
+
+var result = string1.concat(' ' + string2 + ' ' + string3 + ' ' + string4);
+
+console.log(result);
